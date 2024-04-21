8a16737493c98615d72b51b99b156a62a023892e
danzhou <167632054+danzhou01@users.noreply.github.com>
2024-04-21 17:27:26 +0800

    Create metadata.js

---
+++ metadata.js
@@ -0,0 +1,7 @@
+{
+"name": "Analysis",
+"website": "https://github.com/danzhou01",
+"description": "Big data analysis",
+"logo": "https://raw.githubusercontent.com/danzhou01/client-app/main/icon.png",
+"twitter": "https://twitter.com/clientapp"
+}
